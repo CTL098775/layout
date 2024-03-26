8c687b56efc7214110dca9473ebbfce68385e92a
DESKTOP-J8N7KSC\佳桐 <j747300k@gmail.com>
2024-03-26 18:11:09 +0800

    完善畫面

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isShowMenu:boolean = false; 
+
+  toggleMenu(){
+   this.isShowMenu = !this.isShowMenu; 
+  }
 
 }
